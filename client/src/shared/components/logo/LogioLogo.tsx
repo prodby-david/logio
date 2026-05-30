b94363ba4c4a0e6ce1389f3407b5f63ae116752f
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-05-30 20:03:23 +0800

    feat: add shared logio logo

---
+++ client/src/shared/components/logo/LogioLogo.tsx
@@ -0,0 +1,9 @@
+export default function LogioLogo({ className }: { className?: string }) {
+  return (
+    <span
+      className={`text-2xl font-black text-accent-violet ${className}`}
+    >
+      Logio
+    </span>
+  );
+}
